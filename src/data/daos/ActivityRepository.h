3c7122e9274b338d56a372f6c1aa0ca325cb67f7
StafLoker <staf.loker@gmail.com>
2024-01-24 21:57:32 +0100

    feat #8: ActivityRepository

---
+++ src/data/daos/ActivityRepository.h
@@ -0,0 +1,11 @@
+#ifndef SOCIAL_PLANS_MANAGEMENT_ACTIVITYREPOSITORY_H
+#define SOCIAL_PLANS_MANAGEMENT_ACTIVITYREPOSITORY_H
+
+#include "GenericRepository.h"
+#include "../models/spm/Activity.h"
+
+class ActivityRepository : public GenericRepository<Activity> {
+
+};
+
+#endif //SOCIAL_PLANS_MANAGEMENT_ACTIVITYREPOSITORY_H
