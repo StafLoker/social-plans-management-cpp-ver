45937685500666ac0cd52f653a0ad3c6e39bb2e5
StafLoker <staf.loker@gmail.com>
2024-07-01 18:40:06 +0300

    feat: PlanRepository interface

---
+++ src/data/daos/PlanRepository.h
@@ -0,0 +1,11 @@
+#ifndef SOCIAL_PLANS_MANAGEMENT_PLANREPOSITORY_H
+#define SOCIAL_PLANS_MANAGEMENT_PLANREPOSITORY_H
+
+#include "GenericRepository.h"
+#include "../models/spm/Plan.h"
+
+class PlanRepository : public GenericRepository<Plan> {
+
+};
+
+#endif //SOCIAL_PLANS_MANAGEMENT_PLANREPOSITORY_H
