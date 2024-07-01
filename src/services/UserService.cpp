aa2973cc21b5852b05db2f1238dab2eb723e8268
StafLoker <staf.loker@gmail.com>
2024-07-01 23:27:39 +0300

    feat: complete UserService

--- src/services/UserService.cpp
+++ src/services/UserService.cpp
@@ -1,1 +1,27 @@
 #include "UserService.h"
+
+#include "exceptions/SecurityProhibitionException.h"
+#include "exceptions/DuplicateException.h"
+
+const User *UserService::create(const string &username, const string &password, int age, const string &mobile) {
+    User user(username, mobile, password, age);
+    if (this->userRepository->findByName(user.getName()).has_value()) {
+        throw DuplicateException("The username already exists: " + user.getName());
+    }
+    if (this->userRepository->findByMobile(user.getMobile()).has_value()) {
+        throw DuplicateException("The mobile number already exists: " + user.getMobile());
+    }
+    return this->userRepository->create(&user);
+}
+
+void UserService::logIn(const string &username, const string &password) {
+    auto user = this->userRepository->findByName(username);
+    if (!user.has_value() || user.value()->getPassword() != password) {
+        throw SecurityProhibitionException("Unauthorized credentials");
+    }
+    this->session->setLoggedUser(user.value());
+}
+
+void UserService::logOut() {
+    this->session->setLoggedUser(nullptr);
+}
--- src/services/UserService.h
+++ src/services/UserService.h
@@ -2,8 +2,21 @@
 #define SOCIAL_PLANS_MANAGEMENT_USERSERVICE_H
 
 
-class UserService {
+#include "../data/daos/UserRepository.h"
+#include "Session.h"
 
+class UserService {
+private:
+    UserRepository *userRepository;
+    Session *session;
+public:
+    UserService(UserRepository *userRepository, Session *session) : userRepository(userRepository), session(session) {}
+
+    const User* create(const string& username, const string& password, int age, const string& mobile);
+
+    void logIn(const string& username, const string& password);
+
+    void logOut();
 };
 
 
