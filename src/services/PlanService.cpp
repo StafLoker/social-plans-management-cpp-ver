fa5919cc47afdffc6de71e118eea1c065d6076d3
StafLoker <staf.loker@gmail.com>
2024-07-02 09:27:00 +0300

    feat: structure of PlanService class

--- src/services/PlanService.cpp
+++ src/services/PlanService.cpp
@@ -1,1 +1,45 @@
 #include "PlanService.h"
+
+Plan *PlanService::create(const string &name, chrono::system_clock::time_point date, const string &meetingPlace, optional<int> capacity) {
+    return nullptr;
+}
+
+void PlanService::deletePlan(long planId) {
+
+}
+
+Plan *PlanService::addActivity(long planId, long activityId) {
+    return nullptr;
+}
+
+Plan *PlanService::enrollSubscriber(long planId) {
+    return nullptr;
+}
+
+float PlanService::price(long planId) {
+    return 0;
+}
+
+int PlanService::duration(long planId) {
+    return 0;
+}
+
+list<Plan *> PlanService::availablePlans() {
+    return list<Plan *>();
+}
+
+list<Plan *> PlanService::subscribedPlans() {
+    return list<Plan *>();
+}
+
+list<Plan *> PlanService::priceRangePlans(float price, float range) {
+    return list<Plan *>();
+}
+
+list<Plan *> PlanService::weekendPlans() {
+    return list<Plan *>();
+}
+
+list<Plan *> PlanService::plansContainingKeyword(const string &keyword) {
+    return list<Plan *>();
+}
--- src/services/PlanService.h
+++ src/services/PlanService.h
@@ -2,8 +2,44 @@
 #define SOCIAL_PLANS_MANAGEMENT_PLANSERVICE_H
 
 
-class PlanService {
+#include "../data/daos/PlanRepository.h"
+#include "../data/daos/ActivityRepository.h"
+#include "Session.h"
 
+class PlanService {
+private:
+    PlanRepository *planRepository;
+    ActivityRepository *activityRepository;
+    Session *session;
+public:
+    PlanService(PlanRepository *planRepository,
+                ActivityRepository *activityRepository,
+                Session *session) : planRepository(planRepository), activityRepository(activityRepository), session(session) {}
+
+    Plan *create(const string &name, chrono::system_clock::time_point date, const string &meetingPlace, optional<int> capacity);
+
+    void deletePlan(long planId);
+
+    Plan *addActivity(long planId, long activityId);
+
+    Plan *enrollSubscriber(long planId);
+
+    float price(long planId);
+
+    int duration(long planId);
+
+    list<Plan *> availablePlans();
+
+    list<Plan *> subscribedPlans();
+
+    list<Plan *> priceRangePlans(float price, float range);
+
+    list<Plan*> weekendPlans();
+
+    list<Plan*> plansContainingKeyword(const string &keyword);
+
+private:
+    void checkNoTimeCollisionBetweenSubscribedPlans(Plan *plan, User *user);
 };
 
 
