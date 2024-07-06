41a54dfcee0f739036f394bd88628929f62de7d0
StafLoker <staf.loker@gmail.com>
2024-07-06 12:57:24 +0300

    feat: structure of plan commands

---
+++ src/console/commands/plan/crud/DeletePlan.h
@@ -0,0 +1,33 @@
+#ifndef SOCIAL_PLANS_MANAGEMENT_DELETEPLAN_H
+#define SOCIAL_PLANS_MANAGEMENT_DELETEPLAN_H
+
+#include "../../Command.h"
+
+#include "../../../../services/PlanService.h"
+#include "../../../../services/Session.h"
+#include "../../../View.h"
+
+class DeletePlan : public Command {
+private:
+    const string VALUE = "create-user";
+    const string PARAMETER_HELP = "<name>;<password>;<age>;<mobile>";
+    const string COMMENT_HELP = "Register new user";
+
+    Session *session;
+    PlanService *planService;
+    View *view;
+
+public:
+    DeletePlan(Session *session, PlanService *planService, View *view) : session(session), planService(planService), view(view) {}
+
+    void execute(const string *values) override;
+
+    string &value() override;
+
+    string &helpParameters() override;
+
+    string &helpComment() override;
+};
+
+
+#endif //SOCIAL_PLANS_MANAGEMENT_DELETEPLAN_H
---
+++ src/console/commands/plan/list/CostRangePlans.cpp
@@ -0,0 +1,17 @@
+#include "CostRangePlans.h"
+
+void CreateUser::execute(const string *values) {
+
+}
+
+string &CreateUser::value() {
+    return const_cast<string &>(this->VALUE);
+}
+
+string &CreateUser::helpParameters() {
+    return const_cast<string &>(this->PARAMETER_HELP);
+}
+
+string &CreateUser::helpComment() {
+    return const_cast<string &>(this->COMMENT_HELP);
+}
